0a559ffa73e55dfee903ab6147c8856ccdf0b3e8
Thomas Markus <ik@thomasmarkus.nl>
2012-04-14 12:42:07 +0200

    add missing files

---
+++ src/plugins/WebOfTrust/controller/IdentityManagement.java
@@ -0,0 +1,181 @@
+package plugins.WebOfTrust.controller;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+import plugins.WebOfTrust.FCPInterface;
+import plugins.WebOfTrust.IdentityUpdater;
+import plugins.WebOfTrust.IdentityUpdaterRequestClient;
+import plugins.WebOfTrust.WebOfTrust;
+import plugins.WebOfTrust.RequestScheduler;
+import plugins.WebOfTrust.Utils;
+import plugins.WebOfTrust.datamodel.IVertex;
+
+import thomasmarkus.nl.freenet.graphdb.H2Graph;
+
+import freenet.client.FetchException;
+import freenet.client.HighLevelSimpleClient;
+import freenet.client.async.ClientGetCallback;
+import freenet.clients.http.ToadletContext;
+import freenet.clients.http.ToadletContextClosedException;
+import freenet.keys.FreenetURI;
+import freenet.support.api.HTTPRequest;
+
+public class IdentityManagement extends freenet.plugin.web.HTMLFileReaderToadlet {
+	
+	private H2Graph graph;
+	private WebOfTrust main;
+	
+	/**
+	 * The official seed identities of the WoT plugin: If a newbie wants to download the whole offficial web of trust, he needs at least one
+	 * trust list from an identity which is well-connected to the web of trust. To prevent newbies from having to add this identity manually,
+	 * the Freenet development team provides a list of seed identities - each of them is one of the developers.
+	 */
+	private static final String[] SEED_IDENTITIES = new String[] { 
+		"USK@QeTBVWTwBldfI-lrF~xf0nqFVDdQoSUghT~PvhyJ1NE,OjEywGD063La2H-IihD7iYtZm3rC0BP6UTvvwyF5Zh4,AQACAAE/WebOfTrust/90", // xor
+		"USK@z9dv7wqsxIBCiFLW7VijMGXD9Gl-EXAqBAwzQ4aq26s,4Uvc~Fjw3i9toGeQuBkDARUV5mF7OTKoAhqOA9LpNdo,AQACAAE/WebOfTrust/60", // Toad
+		"USK@o2~q8EMoBkCNEgzLUL97hLPdddco9ix1oAnEa~VzZtg,X~vTpL2LSyKvwQoYBx~eleI2RF6QzYJpzuenfcKDKBM,AQACAAE/WebOfTrust/0", // Bombe
+		"USK@cI~w2hrvvyUa1E6PhJ9j5cCoG1xmxSooi7Nez4V2Gd4,A3ArC3rrJBHgAJV~LlwY9kgxM8kUR2pVYXbhGFtid78,AQACAAE/WebOfTrust/19", // TheSeeker
+		"USK@D3MrAR-AVMqKJRjXnpKW2guW9z1mw5GZ9BB15mYVkVc,xgddjFHx2S~5U6PeFkwqO5V~1gZngFLoM-xaoMKSBI8,AQACAAE/WebOfTrust/47", // zidel
+	};
+
+	
+	public IdentityManagement(WebOfTrust main, HighLevelSimpleClient client, String filepath, String URLPath, H2Graph graph) {
+		super(client, filepath, URLPath);
+		this.main = main;
+		this.graph = graph;
+	}
+
+	public void handleMethodGET(URI uri, HTTPRequest request, ToadletContext ctx) throws ToadletContextClosedException, IOException
+	{
+		Document doc = Jsoup.parse(readFile());
+		Element identities_div = doc.select("#identities").first();
+		
+		try {
+			for(long own_vertex : graph.getVertexByPropertyValue(IVertex.OWN_IDENTITY, "true"))
+			{
+				Map<String, List<String>> props = graph.getVertexProperties(own_vertex);
+				
+				Element p = doc.createElement("p");
+				p.text(props.get(IVertex.NAME).get(0));
+				
+				Element form = doc.createElement("form").attr("action", "restore.html").attr("method", "post");
+				Element hiddenValue = doc.createElement("input").attr("type", "hidden").attr("name", "action").attr("value", "delete");
+				Element hiddenID = doc.createElement("input").attr("type", "hidden").attr("name", "id").attr("value", props.get(IVertex.ID).get(0));
+				Element submit = doc.createElement("input").attr("type", "submit").attr("value", "delete");
+				form.appendChild(hiddenValue);
+				form.appendChild(hiddenID);
+				form.appendChild(submit);
+				
+				p.appendChild(form);
+				identities_div.appendChild(p);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	    writeReply(ctx, 200, "text/html", "content", doc.html());
+	}
+	
+	public void handleMethodPOST(URI uri, HTTPRequest request, ToadletContext ctx) throws ToadletContextClosedException, IOException, SQLException, FetchException
+	{
+	    String action = request.getPartAsStringFailsafe("action", 200);
+		
+	    if(action.equals("restore"))
+	    {
+			FreenetURI requestURI = new FreenetURI(request.getPartAsStringFailsafe("requestURI", 20000));
+		    FreenetURI insertURI = new FreenetURI(request.getPartAsStringFailsafe("insertURI", 20000));
+		    restoreIdentity(requestURI, insertURI);
+	    }	
+		else if (action.equals("create"))
+		{
+			String name = request.getPartAsStringFailsafe("name", 2000);
+			createIdentity(name);
+		}
+		else if (action.equals("delete"))
+		{
+			String id = request.getPartAsStringFailsafe("id", 2000);
+			removeIdentity(id);
+		}
+	    
+	    handleMethodGET(uri, request, ctx);
+	}
+
+	private void removeIdentity(String id) throws SQLException {
+		List<Long> vertices = graph.getVertexByPropertyValue(IVertex.ID, id);
+
+		for(long vertex : vertices)
+		{
+			graph.removeVertex(vertex);
+		}
+	}
+
+	private void restoreIdentity(FreenetURI requestURI, FreenetURI insertURI)
+			throws SQLException, FetchException {
+		List<Long> existing_identity = graph.getVertexByPropertyValue("id", Utils.getIDFromKey(requestURI));
+		if (existing_identity.size() == 0) //does identity already exist?
+		{
+			IdentityUpdaterRequestClient rc = new IdentityUpdaterRequestClient();
+			HighLevelSimpleClient hl = main.getHL();
+			RequestScheduler rs = main.getRequestScheduler();
+			ClientGetCallback cc = new IdentityUpdater(rs, graph, hl, true);  
+			
+			addOwnIdentity(requestURI, insertURI);
+			
+			//Fetch the identity from freenet
+			System.out.println("Starting to fetch your own identity");
+			rs.addInFlight(hl.fetch(requestURI, 200000, rc, cc, hl.getFetchContext()));
+		}
+	}
+
+	private void createIdentity(String name) throws SQLException, MalformedURLException {
+		//create ssk keypair
+		FreenetURI[] keypair = main.getHL().generateKeyPair(WebOfTrust.namespace);
+		FreenetURI newRequestURI = keypair[1].setKeyType("USK").setDocName(WebOfTrust.namespace).setSuggestedEdition(0).setMetaString(null);
+		FreenetURI newInsertURI = keypair[0].setKeyType("USK").setDocName(WebOfTrust.namespace).setSuggestedEdition(0).setMetaString(null);
+		
+		//create minimal identity in store
+		long own_identity_vertex = addOwnIdentity(newRequestURI, newInsertURI);
+		
+		//set the name of this identity
+		graph.updateVertexProperty(own_identity_vertex, IVertex.NAME, name);
+		
+		//add trust relations to seed identities
+		for(String key : SEED_IDENTITIES)
+		{
+			FreenetURI seedKey = new FreenetURI(key);
+			String seedID = Utils.getIDFromKey(seedKey);
+			String id = Utils.getIDFromKey(newRequestURI);
+			
+			IdentityUpdater.getPeerIdentity(graph, seedKey); //try to get from the db it and add it otherwise
+			FCPInterface.setTrust(graph, id, seedID, "100", "Initial seed identity");
+		}
+	}
+
+	/**
+	 * add minimal identity features to graph store
+	 * @param requestURI
+	 * @param insertURI
+	 * @throws SQLException
+	 */
+	
+	private long addOwnIdentity(FreenetURI requestURI, FreenetURI insertURI) throws SQLException {
+		long vertex_id = graph.createVertex();
+		graph.updateVertexProperty(vertex_id, IVertex.ID, Utils.getIDFromKey(requestURI));
+		graph.updateVertexProperty(vertex_id, IVertex.OWN_IDENTITY, "true");
+		graph.updateVertexProperty(vertex_id, IVertex.INSERT_URI, insertURI.toASCIIString());
+		graph.updateVertexProperty(vertex_id, IVertex.REQUEST_URI, requestURI.toASCIIString());
+		graph.updateVertexProperty(vertex_id, IVertex.EDITION, "0");
+		return vertex_id;
+	}
+}
---
+++ src/plugins/WebOfTrust/controller/OverviewController.java
@@ -0,0 +1,116 @@
+package plugins.WebOfTrust.controller;
+
+import java.io.IOException;
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import plugins.WebOfTrust.WebOfTrust;
+import plugins.WebOfTrust.ScoreComputer;
+import plugins.WebOfTrust.datamodel.IVertex;
+
+import thomasmarkus.nl.freenet.graphdb.H2Graph;
+
+import freenet.client.HighLevelSimpleClient;
+import freenet.client.async.ClientGetter;
+import freenet.clients.http.ToadletContext;
+import freenet.clients.http.ToadletContextClosedException;
+import freenet.support.api.HTTPRequest;
+
+public class OverviewController extends freenet.plugin.web.HTMLFileReaderToadlet {
+
+	private H2Graph graph;
+	private WebOfTrust main;
+	
+	public OverviewController(WebOfTrust main, HighLevelSimpleClient client, String filepath, String URLPath, H2Graph graph) {
+		super(client, filepath, URLPath);
+		this.main = main;
+		this.graph = graph;
+	}
+
+	public void handleMethodGET(URI uri, HTTPRequest request, ToadletContext ctx) throws ToadletContextClosedException, IOException
+	{
+		try
+		{
+		    Document doc = Jsoup.parse(readFile());
+			Element stats_div = doc.select("#stats").first();
+			
+			long count_vertices = graph.getVertexCount();
+			long count_edges = graph.getEdgeCount();
+			
+			stats_div.text("Number of identities: " + count_vertices).append("<br />");
+			stats_div.append("Number of trust relations: " + count_edges + "<br />");
+			stats_div.append("Number of requests in flight currently: " + main.getRequestScheduler().getInFlightSize() + "<br />");
+			stats_div.append("Backlog: " + main.getRequestScheduler().getBacklogSize() + "<br />");
+			
+
+			stats_div.append("Own identities in local storage: "  + "<br />");
+			stats_div.append("<ul>");
+			for(long identity : graph.getVertexByPropertyValue("ownIdentity", "true"))
+			{
+				Map<String, List<String>> props = graph.getVertexProperties(identity);
+				if (props.containsKey(IVertex.NAME))
+				{
+					stats_div.append("<li>" + props.get(IVertex.NAME).get(0) + "  (" + props.get("id").get(0) + ")</li>");	
+				}
+			}
+			stats_div.append("</ul><br />");
+			
+			stats_div.append("URIs currently in flight: "  + "<br />");
+			stats_div.append("<ul>");
+			for(ClientGetter cg : main.getRequestScheduler().getInFlight())
+			{
+				stats_div.append("<li>" + cg.getURI() + " finished: " + cg.isFinished() + "</li>");	
+			}
+			stats_div.append("</ul>");
+			
+			addTrustInformation(stats_div);
+			
+			writeReply(ctx, 200, "text/html", "content", doc.html());
+			
+			//ScoreComputer sc = new ScoreComputer(graph);
+			//sc.compute("zALLY9pbzMNicVn280HYqS2UkK0ZfX5LiTcln-cLrMU,GoLpCcShPzp3lbQSVClSzY7CH9c9HTw0qRLifBYqywY,AQACAAE");
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	public void addTrustInformation(Element stats_div)
+	{
+        
+		//parse it and determine WHO you trust
+        
+        // TrustList/Trust
+        // Identity, value
+        
+        //traverse the orientdb and get the number of identities.
+
+		
+	}
+	
+	
+	
+	public void handleMethodPOST(URI uri, HTTPRequest request, ToadletContext ctx) throws ToadletContextClosedException, IOException, SQLException
+	{
+		String action = request.getPartAsStringFailsafe("action", 20000);
+		
+		//generate the web of trust for each of the ownIdentities that we have
+		if (action.equals("generate"))
+		{
+			ScoreComputer sc = new ScoreComputer(graph);
+			for(long vertex_id : graph.getVertexByPropertyValue("ownIdentity", "true"))
+			{
+				Map<String, List<String>> props = graph.getVertexProperties(vertex_id);
+				sc.compute(props.get("id").get(0));
+			}
+		}
+		handleMethodGET(uri, request, ctx);
+	}
+}
