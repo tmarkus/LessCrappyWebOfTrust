273706306824fa28f7339de7d86ec842eeeaafd0
Thomas Markus <ik@thomasmarkus.nl>
2012-05-06 22:08:58 +0200

    Support modifying/deleting properties from a local identity

--- src/plugins/WebOfTrust/pages/ShowIdentityController.java
+++ src/plugins/WebOfTrust/pages/ShowIdentityController.java
@@ -20,7 +20,6 @@ import thomasmarkus.nl.freenet.graphdb.Edge;
 import thomasmarkus.nl.freenet.graphdb.H2Graph;
 import thomasmarkus.nl.freenet.graphdb.H2GraphFactory;
 
-import freenet.client.FetchException;
 import freenet.client.HighLevelSimpleClient;
 import freenet.clients.http.ToadletContext;
 import freenet.clients.http.ToadletContextClosedException;
@@ -41,6 +40,7 @@ public class ShowIdentityController extends freenet.plugin.web.HTMLFileReaderToa
 		try
 		{
 			graph = gf.getGraph();
+
 			//existing patterns
 		    Document doc = Jsoup.parse(readFile());
 			
@@ -51,28 +51,74 @@ public class ShowIdentityController extends freenet.plugin.web.HTMLFileReaderToa
 			final String id = request.getParam("id");
 			
 			//get the identity vertex & properties
-			Long id_vertex = graph.getVertexByPropertyValue(IVertex.ID, id).get(0);
-			Map<String, List<String>> props = graph.getVertexProperties(id_vertex);
+			final Long id_vertex = graph.getVertexByPropertyValue(IVertex.ID, id).get(0);
+			final Map<String, List<String>> props = graph.getVertexProperties(id_vertex);
+			final boolean is_own_identity = props.containsKey(IVertex.OWN_IDENTITY);
 			
 			info_div.append("<h1>Identity properties</h1>");
 			SortedSet<String> sortedKeys = new TreeSet<String>(props.keySet());
 			
+			Element propertiesForm = doc.createElement("form").attr("action", "#").attr("method", "post");
+			propertiesForm.appendChild(doc.createElement("input").attr("type", "hidden").attr("name", "action").val("modify_properties"));
+			propertiesForm.appendChild(doc.createElement("input").attr("type", "hidden").attr("name", "identity").val(id));
+
 			Element table = doc.createElement("table");
+			propertiesForm.appendChild(table);
 			table.appendChild(
 								doc.createElement("tr").appendChild(
 										doc.createElement("th").text("Name")).appendChild(
 										doc.createElement("th").text("Value")));
 			
 			//generic properties associated with this identity in the graph store
+			int property_index = 0;
 			for(String key : sortedKeys)
 			{
 				for(String value : props.get(key))
 				{
-					table.appendChild(doc.createElement("tr").appendChild(doc.createElement("td").text(key)).appendChild(doc.createElement("td").text(value)));
+					if (is_own_identity)
+					{
+						Element propertyName = doc.createElement("input").attr("type", "text").attr("name", "propertyName"+property_index).val(key);
+						Element propertyValue = doc.createElement("input").attr("type", "text") .attr("name", "propertyValue"+property_index).val(value).attr("size", "100");
+						Element oldPropertyName = doc.createElement("input").attr("type", "hidden") .attr("name", "oldPropertyName"+property_index).val(key);
+						Element oldPropertyValue = doc.createElement("input").attr("type", "hidden") .attr("name", "oldPropertyValue"+property_index).val(value);
+						
+						Element tr = doc.createElement("tr");
+						table.appendChild(tr);
+						tr.appendChild(doc.createElement("td").appendChild(propertyName));
+						tr.appendChild(doc.createElement("td").appendChild(propertyValue));
+						
+						tr.appendChild(oldPropertyName);
+						tr.appendChild(oldPropertyValue);
+					}
+					else
+					{
+						table.appendChild(doc.createElement("tr").appendChild(doc.createElement("td").text(key)).appendChild(doc.createElement("td").text(value)));	
+					}
+				
+					property_index += 1;
 				}
 			}
-			info_div.appendChild(table);
+
+			//extra empty property value pair
+			if (is_own_identity)
+			{
+				Element propertyName = doc.createElement("input").attr("type", "text").attr("name", "propertyName");
+				Element propertyValue = doc.createElement("input").attr("type", "text") .attr("name", "propertyValue").attr("size", "100");
+				
+				Element tr = doc.createElement("tr");
+				table.appendChild(tr);
+				tr.appendChild(doc.createElement("td").appendChild(propertyName));
+				tr.appendChild(doc.createElement("td").appendChild(propertyValue));
+			}
+
 			
+			//submit button
+			if (is_own_identity)
+			{
+				Element modifySubmit = doc.createElement("input").attr("type", "submit").val("Modify");
+				propertiesForm.appendChild(modifySubmit);
+			}
+			info_div.appendChild(propertiesForm);
 			
 			//allow specifying an updated trust value
 			info_div.append("<h1>Local trust assignments to this identity:</h1>");
@@ -226,29 +272,8 @@ public class ShowIdentityController extends freenet.plugin.web.HTMLFileReaderToa
 				final long edge_id = Long.parseLong(request.getPartAsStringFailsafe("edge_id", 1000));
 				graph.removeEdge(edge_id);
 			}
-			else if (action.equals("set_trust"))
-			{
-				String own_identity = request.getPartAsStringFailsafe("own_identity_id", 1000);
-				String identity = request.getPartAsStringFailsafe("identity_id", 1000);
-				String trustValue = request.getPartAsStringFailsafe("trust_value", 1000);
-				String trustComment = request.getPartAsStringFailsafe("trust_comment", 1000);
-			
-				List<Long> own_vertices = graph.getVertexByPropertyValue(IVertex.ID, own_identity);
-				List<Long> identity_vertices = graph.getVertexByPropertyValue(IVertex.ID, identity);
-				
-				for(long own_vertex : own_vertices)
-				{
-					for(long identity_vertex : identity_vertices)
-					{
-						long edge;
-						try						{ edge = graph.getEdgeByVerticesAndProperty(own_vertex, identity_vertex, IEdge.SCORE); }
-						catch(SQLException e) 	{ edge = graph.addEdge(own_vertex, identity_vertex); }
-
-						graph.updateEdgeProperty(edge, IEdge.SCORE, trustValue);
-						graph.updateEdgeProperty(edge, IEdge.COMMENT, trustComment);
-					}
-				}
-			}
+			else if (action.equals("set_trust"))	setTrust(request, graph);
+			else if (action.equals("modify_properties")) modifyProperties(request, graph);
 		}
 		finally
 		{
@@ -256,6 +281,53 @@ public class ShowIdentityController extends freenet.plugin.web.HTMLFileReaderToa
 		}
 
 		handleMethodGET(uri, request, ctx);
+	}
+
+	private void modifyProperties(HTTPRequest request, H2Graph graph) throws SQLException 
+	{
+		final String id = request.getPartAsStringFailsafe("identity", 1000);
+		final long id_vertex = graph.getVertexByPropertyValue(IVertex.ID, id).get(0);
+		
+		int i = 0;
+		while(request.isPartSet("oldPropertyName"+i))
+		{
+			String oldPropertyName = request.getPartAsStringFailsafe("oldPropertyName"+i, 10000);
+			String oldPropertyValue = request.getPartAsStringFailsafe("oldPropertyValue"+i, 10000);
+			String propertyName = request.getPartAsStringFailsafe("propertyName"+i, 10000);
+			String propertyValue = request.getPartAsStringFailsafe("propertyValue"+i, 10000);
+			
+			graph.removeVertexPropertyValue(id_vertex, oldPropertyName, oldPropertyValue);
+			if (!propertyName.trim().equals(""))
+			{
+				graph.addVertexProperty(id_vertex, propertyName, propertyValue);	
+			}
+		
+			i += 1;
+		}
+	}
+
+	protected void setTrust(HTTPRequest request, H2Graph graph)
+			throws SQLException {
+		String own_identity = request.getPartAsStringFailsafe("own_identity_id", 1000);
+		String identity = request.getPartAsStringFailsafe("identity_id", 1000);
+		String trustValue = request.getPartAsStringFailsafe("trust_value", 1000);
+		String trustComment = request.getPartAsStringFailsafe("trust_comment", 1000);
+
+		List<Long> own_vertices = graph.getVertexByPropertyValue(IVertex.ID, own_identity);
+		List<Long> identity_vertices = graph.getVertexByPropertyValue(IVertex.ID, identity);
+		
+		for(long own_vertex : own_vertices)
+		{
+			for(long identity_vertex : identity_vertices)
+			{
+				long edge;
+				try						{ edge = graph.getEdgeByVerticesAndProperty(own_vertex, identity_vertex, IEdge.SCORE); }
+				catch(SQLException e) 	{ edge = graph.addEdge(own_vertex, identity_vertex); }
+
+				graph.updateEdgeProperty(edge, IEdge.SCORE, trustValue);
+				graph.updateEdgeProperty(edge, IEdge.COMMENT, trustComment);
+			}
+		}
 	}
 	
 	@Override
