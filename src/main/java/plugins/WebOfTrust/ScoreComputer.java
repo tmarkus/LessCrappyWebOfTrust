4c4f2c032a7b6497d60f93093e8c0601365238ea
Thomas Markus <ik@thomasmarkus.nl>
2012-09-27 15:56:55 +0200

    don't throw away index too early

--- src/main/java/plugins/WebOfTrust/ScoreComputer.java
+++ src/main/java/plugins/WebOfTrust/ScoreComputer.java
@@ -34,72 +34,80 @@ public class ScoreComputer {
 	{
 		System.out.println("Starting trust calculations...");
 
-		synchronized (db) {
+		//delete the calculated trust values for all identities for this ownIdentity
+		final IndexHits<Node> own_identities = nodeIndex.get(IVertex.OWN_IDENTITY, true);
+		for(Node own_identity : own_identities)
+		{
+			System.out.println("Calculating for: " + own_identity.getProperty(IVertex.ID));
 
-			//delete the calculated trust values for all identities for this ownIdentity
-			final IndexHits<Node> own_identities = nodeIndex.get(IVertex.OWN_IDENTITY, true);
-			for(Node own_identity : own_identities)
+			final String trustProperty = IVertex.TRUST+"."+own_identity.getProperty(IVertex.ID);
+			final String distanceProperty = IVertex.DISTANCE + "." + own_identity.getProperty(IVertex.ID);
+
+			//assign 100 trust to local identity
+			initOwnIdentity(own_identity, trustProperty, distanceProperty, nodeIndex);
+
+			//calcuate the distance of every identity for this own_identity
+			Index<Node> distanceIndex = calculateDistances(own_identity, distanceProperty);
+
+			//local overrides for trust values
+			localOverrides(own_identity, trustProperty);
+
+			for(byte distance=2; distance < 7; distance++)
 			{
-				System.out.println("Calculating for: " + own_identity.getProperty(IVertex.ID));
+				System.out.println("Current distance = " + distance);
 
-				final String trustProperty = IVertex.TRUST+"."+own_identity.getProperty(IVertex.ID);
-				final String distanceProperty = IVertex.DISTANCE + "." + own_identity.getProperty(IVertex.ID);
-
-				//assign 100 trust to local identity
-				initOwnIdentity(own_identity, trustProperty, distanceProperty, nodeIndex);
-
-				//calcuate the distance of every identity for this own_identity
-				Index<Node> distanceIndex = calculateDistances(own_identity, distanceProperty);
-
-				//local overrides for trust values
-				localOverrides(own_identity, trustProperty);
-				
-				for(byte distance=2; distance < 7; distance++)
+				Transaction tx = db.beginTx();
+				try
 				{
-					System.out.println("Current distance = " + distance);
-
-					Transaction tx = db.beginTx();
-					try
+					IndexHits<Node> layer = distanceIndex.get(distanceProperty, distance);
+					for(Node current_node : layer)	
 					{
-						IndexHits<Node> layer = distanceIndex.get(distanceProperty, distance);
-						for(Node current_node : layer)	
+						//calculate summed trust for all identities that assign trust to this identity with a lower distance
+						int avg_score = 0;
+						boolean changed = false;
+						for(Relationship rel : current_node.getRelationships(Direction.INCOMING, Rel.TRUSTS))
 						{
-							//calculate summed trust for all identities that assign trust to this identity with a lower distance
-							int avg_score = 0;
-							boolean changed = false;
-							for(Relationship rel : current_node.getRelationships(Direction.INCOMING, Rel.TRUSTS))
+							final Node startNode = rel.getStartNode(); 
+							if (startNode.hasProperty(trustProperty) &&
+									(Integer) startNode.getProperty(trustProperty) > 0 &&
+									startNode.hasProperty(distanceProperty))
 							{
-								final Node startNode = rel.getStartNode(); 
-								if (startNode.hasProperty(trustProperty) &&
-										(Integer) startNode.getProperty(trustProperty) > 0 &&
-										startNode.hasProperty(distanceProperty))
-								{
-									final byte score = (Byte) rel.getProperty(IEdge.SCORE);
-									final byte distanceValue = (Byte) startNode.getProperty(distanceProperty);
-									changed = true;
+								final byte score = (Byte) rel.getProperty(IEdge.SCORE);
+								final byte distanceValue = (Byte) startNode.getProperty(distanceProperty);
+								changed = true;
 
-									avg_score += Math.round(score*Math.pow(decay, distanceValue-1));
-								}
-							}
-							
-							if (changed && 
-									!current_node.hasProperty(trustProperty) ||
-									!current_node.getProperty(trustProperty).equals(avg_score))
-							{
-								current_node.setProperty(trustProperty, avg_score);
+								avg_score += Math.round(score*Math.pow(decay, distanceValue-1));
 							}
 						}
-						
-						tx.success();
+
+						if (changed && 
+								!current_node.hasProperty(trustProperty) ||
+								!current_node.getProperty(trustProperty).equals(avg_score))
+						{
+							current_node.setProperty(trustProperty, avg_score);
+						}
 					}
-					finally
-					{
-						tx.finish();
-					}
+
+					tx.success();
+				}
+				finally
+				{
+					tx.finish();
 				}
 			}
-		}
 		
+			Transaction tx = db.beginTx();
+			try
+			{
+				distanceIndex.delete();
+				tx.success();
+			}
+			finally
+			{
+				tx.finish();
+			}
+		}
+
 		System.out.println("Done with trust calculation.");
 	}
 
@@ -131,12 +139,12 @@ public class ScoreComputer {
 
 		System.out.println("Setting the distance for all identities");
 		Index<Node> distanceIndex = null;
-		
+
 		Transaction tx = db.beginTx();
 		try
 		{
 			distanceIndex = db.index().forNodes( "distance" );
-			
+
 			for(Path path : td.traverse(own_identity))
 			{
 				final Node current_node = path.endNode();
@@ -152,7 +160,6 @@ public class ScoreComputer {
 		}
 		finally
 		{
-			if (distanceIndex != null) distanceIndex.delete();
 			tx.finish();
 		}
 		System.out.println("Distance setting completed for all identities");
@@ -166,7 +173,7 @@ public class ScoreComputer {
 	 * @param distanceProperty
 	 * @param nodeIndex
 	 */
-	
+
 	protected void initOwnIdentity(Node own_vertex, final String trustProperty, final String distanceProperty, ReadableIndex<Node> nodeIndex) {
 		Transaction tx = db.beginTx();
 		try
