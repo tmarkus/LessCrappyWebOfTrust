621ac87eecdd2558aa88d693d62ef17a3a9be21e
SeekingFor <SeekingFor@i2pmail.org>
2012-11-28 16:27:43 +0000

    first test with toadlet instead of template engine for main overview page

--- src/main/java/plugins/WebOfTrust/pages/OverviewController.java
+++ src/main/java/plugins/WebOfTrust/pages/OverviewController.java
@@ -3,12 +3,13 @@ package plugins.WebOfTrust.pages;
 import java.io.IOException;
 import java.net.URI;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.index.ReadableIndex;
 import org.neo4j.tooling.GlobalGraphOperations;
 
 import plugins.WebOfTrust.WebOfTrust;
@@ -17,18 +18,27 @@ import plugins.WebOfTrust.datamodel.Rel;
 
 
 import freenet.client.HighLevelSimpleClient;
+import freenet.clients.http.PageNode;
+import freenet.clients.http.Toadlet;
 import freenet.clients.http.ToadletContext;
 import freenet.clients.http.ToadletContextClosedException;
+import freenet.support.HTMLNode;
 import freenet.support.api.HTTPRequest;
 
-public class OverviewController extends freenet.plugin.web.HTMLFileReaderToadlet {
-
+public class OverviewController extends Toadlet {
+	protected String path;
+	protected String filePath;
+	protected GraphDatabaseService db;
+	protected ReadableIndex<Node> nodeIndex;
 	private WebOfTrust main;
 	
 	public OverviewController(WebOfTrust main, HighLevelSimpleClient client, String filepath, String URLPath, GraphDatabaseService db) {
-		super(client, main.getDB(), filepath, URLPath);
-		this.main = main;
+		super(client);
+		this.path = URLPath;
+		this.filePath = filepath;
 		this.db = db;
+		
+		nodeIndex = db.index().getNodeAutoIndexer().getAutoIndex();
 	}
 
 	public void handleMethodGET(URI uri, HTTPRequest request, ToadletContext ctx) throws ToadletContextClosedException, IOException
@@ -37,10 +47,11 @@ public class OverviewController extends freenet.plugin.web.HTMLFileReaderToadlet
 			writeHTMLReply(ctx, 403, "forbidden", "Your host is not allowed to access this page.");
 			return;
 		}
+		PageNode mPageNode = ctx.getPageMaker().getPageNode("LCWoT - overview", true, true, ctx);
 		try
 		{
-			Document doc = Jsoup.parse(readFile());
-			Element stats_div = doc.select("#stats").first();
+//			Document doc = Jsoup.parse(readFile());
+//			Element stats_div = doc.select("#stats").first();
 
 			long count_identities = 0;
 			long count_trust_relations = 0;
@@ -54,44 +65,56 @@ public class OverviewController extends freenet.plugin.web.HTMLFileReaderToadlet
 			{
 				if (rel.isType(Rel.TRUSTS)) count_trust_relations +=1;
 			}
-			
-			Element list = doc.createElement("ul");
-			
-			list.appendChild(doc.createElement("li").text("Number of identities: " + count_identities));
-			list.appendChild(doc.createElement("li").text("Number of trust relations: " + count_trust_relations));
-			list.appendChild(doc.createElement("li").text("Number of requests in flight currently: " + main.getRequestScheduler().getInFlightSize()));
-			list.appendChild(doc.createElement("li").text("Backlog: " + main.getRequestScheduler().getBacklogSize()));
-			list.appendChild(doc.createElement("li").text("Number of active db connections: " + 666));
-			
-			stats_div.appendChild(list);
+			HTMLNode list = new HTMLNode("ul");
+			list.addChild("li", "Number of identities: " + count_identities);
+			list.addChild("li", "Number of trust relations: " + count_trust_relations);
+			list.addChild("li", "Number of requests in flight currently: " + main.getRequestScheduler().getInFlightSize());
+			list.addChild("li", "Backlog: " + main.getRequestScheduler().getBacklogSize());
+			list.addChild("li", "Number of active db connections: " + 666);
+//			Element list = doc.createElement("ul");
+//			
+//			list.appendChild(doc.createElement("li").text("Number of identities: " + count_identities));
+//			list.appendChild(doc.createElement("li").text("Number of trust relations: " + count_trust_relations));
+//			list.appendChild(doc.createElement("li").text("Number of requests in flight currently: " + main.getRequestScheduler().getInFlightSize()));
+//			list.appendChild(doc.createElement("li").text("Backlog: " + main.getRequestScheduler().getBacklogSize()));
+//			list.appendChild(doc.createElement("li").text("Number of active db connections: " + 666));
+			mPageNode.content.addChild(list);
+//			stats_div.appendChild(list);
 
-			stats_div.append("<h2> Own identities in local storage </h2>");
-			Element own_identities = doc.createElement("ul");
-			
+			mPageNode.content.addChild("h2", "Own identities in local storage");
+//			stats_div.append("<h2> Own identities in local storage </h2>");
+//			Element own_identities = doc.createElement("ul");
+			list = new HTMLNode("ul");
 			for(Node identity : nodeIndex.get(IVertex.OWN_IDENTITY, true))
 			{
 				if (identity.hasProperty(IVertex.NAME) )
 				{
-					own_identities.appendChild(doc.createElement("li").text((String) identity.getProperty(IVertex.NAME) + "  (" + (String) identity.getProperty(IVertex.ID) + ")"));
+					list.addChild("li", (String) identity.getProperty(IVertex.NAME) + "  (" + (String) identity.getProperty(IVertex.ID) + ")");
+//					own_identities.appendChild(doc.createElement("li").text((String) identity.getProperty(IVertex.NAME) + "  (" + (String) identity.getProperty(IVertex.ID) + ")"));
 				}
 			}
 
-			stats_div.appendChild(own_identities);
+			mPageNode.content.addChild(list);
+//			stats_div.appendChild(own_identities);
 
-			
-			stats_div.append("<h2>URIs currently in flight</h2>");
-			Element inflight = doc.createElement("ol");
+			mPageNode.content.addChild("h2", "URIs currently in flight");
+//			stats_div.append("<h2>URIs currently in flight</h2>");
+//			Element inflight = doc.createElement("ol");
+			list = new HTMLNode("ol");
 			
 			synchronized (main.getRequestScheduler().getInFlight()) {
 				for(String in : main.getRequestScheduler().getInFlight())
 				{
-					inflight.appendChild(doc.createElement("li").text(in));
+//					inflight.appendChild(doc.createElement("li").text(in));
+					list.addChild("li", in);
 				}
 			}
 			
-			stats_div.appendChild(inflight);
+			mPageNode.content.addChild(list);
+//			stats_div.appendChild(inflight);
 			
-			writeReply(ctx, 200, "text/html", "content", doc.html());
+//			writeReply(ctx, 200, "text/html", "content", doc.html());
+			writeReply(ctx, 200, "text/html", "OK", mPageNode.outer.generate());
 		}
 		catch(Exception ex)
 		{
@@ -102,8 +125,13 @@ public class OverviewController extends freenet.plugin.web.HTMLFileReaderToadlet
 		}
 	}
 	
+//	@Override
+//	public void terminate() {
+//		
+//	}
+
 	@Override
-	public void terminate() {
-		
+	public String path() {
+		return filePath;
 	}
 }
